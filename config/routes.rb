79f5f9a6f3f03f8598362271434f6d3fc8f23273
John Chipouras <chipouras@gmail.com>
2015-05-07 19:16:49 -0400

    adding path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'categories#index'
   resources :categories
   resources :exercises
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
